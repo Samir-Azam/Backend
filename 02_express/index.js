e14a78d651c8bc9f304000607301065460c3a6f6
Samir Azam <azamsamir190@gmail.com>
2025-06-12 19:44:58 +0530

    Advance logger

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,15 +1,34 @@
 import express from 'express'
 import "dotenv/config";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express()
-const port =  process.env.PORT || 3000
+const port =  process.env.PORT || 8000
 const teaData = []
 let teaId = 1
-
 app.use(express.json());
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 // Adding new tea
 app.post('/teas',(req,res)=>{
+    logger.info("This is an info message");
     const {name,price} = req.body;
     if (!name || !price){
         return res.status(400).send(`Name and Price is required.`);
@@ -20,7 +39,7 @@ app.post('/teas',(req,res)=>{
         price,
     }
     teaData.push(teaDetails);
-    res.status(200).json(teaDetails);
+    res.status(200).send(teaDetails);
 })
 
 // Get All teas
